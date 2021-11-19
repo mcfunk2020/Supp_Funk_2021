28d2394372dc5b5b427d403ab5026026336a1bdd
Jan Gleixner <jan.gleixner@gmail.com>
2021-11-19 14:46:06 +0100

    tweak plots
    as requested by @mcfunk2020

--- scRNA-Seq-invivo/helper_functions_and_colors.R
+++ scRNA-Seq-invivo/helper_functions_and_colors.R
@@ -108,19 +108,22 @@ scale_fill_age_dark <- function(..., values = age_colors_dark) scale_fill_age(..
 
 volcano_plot <- function(dt, alpha = 0.1, highlight = rank(padj)<=20, color = "black", box.padding = 0.25, label_size = 4, ...){
   if(missing(alpha)) message(paste0("FDR <= alpha = ", alpha))
-  labelled_subset_dt <- eval(substitute(copy(dt)[!(highlight), gene := ""]))
   dt <- copy(dt)
   dt[padj > alpha, sir.class := "not significant"]
   dt[padj <= alpha & -log2FoldChange > 0, sir.class := "up regulated"]
   dt[padj <= alpha & -log2FoldChange < 0, sir.class := "down regulated"]
+  labelled_subset_dt <- eval(substitute(copy(dt)[!(highlight), gene := ""]))
   
   dt %>% ggplot(aes(x = -log2FoldChange, y = -log10(pvalue))) +
     #geom_vline(xintercept = 0, linetype = "dotted") +
-    geom_point(aes(color = sir.class)) +
+    geom_vline(xintercept = 0, linetype = "dashed") +
+    ggrastr::rasterize(geom_point(aes(color = sir.class))) +
+    geom_point(data = dt[sir.class != "not significant",], aes(color = sir.class)) +
     scale_color_manual(breaks = c("down regulated", "up regulated", "not significant"), values = c(maja_blue, maja_red, maja_gray)) +
     guides(color = guide_legend(title = element_blank())) +
     labs(x = "log2fc aged/young") +
-    if(nrow(labelled_subset_dt)>0) ggrepel::geom_text_repel(aes(label = gene), data = labelled_subset_dt, color = color, min.segment.length = 0, size = label_size, box.padding = box.padding, max.overlaps = Inf, ...) else NULL +
+    # geom_vline(xintercept = c(0.5, -0.5), linetype = "dotted") + geom_hline(yintercept = 2, linetype = "dotted") +
+    if(nrow(labelled_subset_dt)>0) ggrepel::geom_label_repel(aes(label = gene), data = labelled_subset_dt, color = color, min.segment.length = 0, size = label_size, box.padding = box.padding, max.overlaps = Inf, ...) else NULL +
     NULL
 }
 
